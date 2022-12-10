d40715ccbafe21938710e728daecb345b63ce45e
zukangy <zukangy1@gmail.com>
2022-12-09 22:34:20 -0800

    Added more endpoints

--- app.py
+++ app.py
@@ -1,11 +1,36 @@
 import uuid
 from flask import Flask, request
+from flask_smorest import abort
 
 from db import stores, items
 
 app = Flask(__name__)
 
 
-@app.get("/stores")
-def get_store():
+@app.get("/store")
+def get_stores():
     return {"stores": list(stores.values())}
+
+
+@app.post("/store")
+def create_store():
+    store_data = request.get_json()
+    if "name" not in store_data:
+        abort(400, message="Bad request. Ensure 'name' is included in the JSON payload.")
+
+    for store in stores.values():
+        if store['name'] == store_data['name']:
+            abort(400, message="Store already exists.")
+
+    store_id = uuid.uuid4().hex
+    new_store = {**store_data, 'id': store_id}
+    stores[store_id] = new_store
+    return new_store
+
+
+@app.get("/store/<string:store_id>")
+def get_store(store_id):
+    try:
+        return stores[store_id]
+    except KeyError:
+        abort(400, message="Store not found.")
